8e703f7d5e6290b898708c9e1b8f16529d97472d
bellesbae <tsowehh@gmail.com>
2022-12-04 14:27:19 +0300

    добавил функции с созвона
    но они ругаются теперь почему-то, Алексей щас посмотрит и починит

--- write_through_cache/src/main/java/com/crafttalk/sampleChat/MainActivity.kt
+++ write_through_cache/src/main/java/com/crafttalk/sampleChat/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.crafttalk.sampleChat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.core.Storage
+import com.example.core.toData
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.Serializable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    val res = WtRepository(WtStorage()).getTestData(2)
+
+    }
+
+class BodyTest(
+    val article_page: Int
+): Serializable
+
+class ResultTest(
+    val res_article: String
+): Serializable
+
+class WtStorage: Storage<ResultTest>() {
+
+    override fun getDataFromRemote(args: Map<String, Any>): ResultTest {
+        val page = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            args.getOrDefault("article_page", 1)
+        } else {
+            args["page"]
+        }
+        return ResultTest(
+            when (page) {
+                1 -> """asdasda das dasd sa da"""
+                2 -> """2"""
+                else -> "Hello"
+            }
+        )
+    }
+
+
+class WtRepository(
+    private val wtStorage: WtStorage
+) {
+    fun getTestData(page: Int): ResultTest? {
+        return wtStorage.getData(
+            mapOf("article_page" to page)
+        )
     }
 }
